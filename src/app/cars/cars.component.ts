0ecf85aac4e84fd1794b3c5f689337ea3d6700e4
Alekxandr <alkv84@yandex.ru>
2018-03-01 10:25:06 +0200

    dinamic data in cars

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
 })
 export class CarsComponent {
 
+  cars: any = [
+    {
+      carName: 'Ford Focus',
+      carYear: 2012,
+      carDescription: 'I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.',
+      carImage: 'https://owner.ford.com/content/dam/assets/ford/personalization/1656/escape.png'
+    },
+    {
+      carName: 'Ford Mustang',
+      carYear: 2011,
+      carDescription: 'I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.',
+      carImage: 'https://owner.ford.com/content/dam/assets/ford/personalization/1656/escape.png'
+    },
+    {
+      carName: 'Ford Mondeo',
+      carYear: 2012,
+      carDescription: 'I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.',
+      carImage: 'https://owner.ford.com/content/dam/assets/ford/personalization/1656/escape.png'
+    }
+  ]
+
   canAddCar: boolean = false;
 
   constructor(){
