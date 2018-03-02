6b174a22f1cfb1459f072379a03b72ae219f378d
Alekxandr <alkv84@yandex.ru>
2018-03-02 17:19:47 +0200

    -

--- src/app/to-do/to-do.component.ts
+++ src/app/to-do/to-do.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-to-do',
@@ -9,13 +9,15 @@ export class ToDoComponent {
   tasks: any[] = [];
 
   addTask(event: Event) {
-    this.tasks.push({
-      message: (<HTMLInputElement>event.target).value
-    });
-    (<HTMLInputElement>event.target).value = '';
+    if (((<HTMLInputElement>event.target).value).length > 3) {
+      this.tasks.push({
+        message: (<HTMLInputElement>event.target).value
+      });
+      (<HTMLInputElement>event.target).value = '';
+    }
   }
 
-  deleteTask(task){
+  deleteTask(task) {
     this.tasks.splice(this.tasks.indexOf(task), 1);
   }
 }
